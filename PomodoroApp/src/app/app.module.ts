0b8c608939c55bf3a7cac55ba8b06de867c649a4
Brayanmcz <brayan.mcz@gmail.com>
2018-04-20 01:45:43 -0700

    Added users-login page component

--- PomodoroApp/src/app/app.module.ts
+++ PomodoroApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { UsersLoginComponent } from './users/users-login/users-login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersLoginComponent
   ],
   imports: [
     BrowserModule,
