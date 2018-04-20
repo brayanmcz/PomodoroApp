d98b7220208cd5f25b1e86e650c8b95bec60dd61
Brayanmcz <brayan.mcz@gmail.com>
2018-04-20 03:02:48 -0700

    Updated imports

--- PomodoroApp/src/app/app.module.ts
+++ PomodoroApp/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthGuardService } from './core/auth-guard.service';
+
 import { UsersLoginComponent } from './users/users-login/users-login.component';
 import { UsersProfileComponent } from './users/users-profile/users-profile.component';
-
 
 @NgModule({
   declarations: [
@@ -16,9 +18,13 @@ import { UsersProfileComponent } from './users/users-profile/users-profile.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [
+    AuthGuardService, 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
